f8b8f16285c2c3deb44ccd3a86e27235c99d6b4a
Luis Herranz <luisherranz@gmail.com>
2016-11-04 17:15:47 +0100

    Reset package.json and remove worona-packages to move it to the npm package

--- worona-packages/entry.js
+++
@@ -1,7 +0,0 @@
-require('babel-polyfill');
-var pkg = require('../src/dashboard').default;
-var packageDevelopment = require('worona-deps').packageDevelopment;
-
-packageDevelopment(pkg);
-
-console.log('Development package loaded!');
