e11835c2c0be63577c6a80a220304a429b3731e0
Luis Herranz <luisherranz@gmail.com>
2016-12-28 16:44:32 +0100

    Add reducers to the example.

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 import * as sagas from './sagas';
+import * as reducers from './reducers';
 
 export {
   sagas,
+  reducers,
 };
---
+++ src/app/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+export const test = () => false;
+
+export default () => combineReducers({
+  test,
+});
