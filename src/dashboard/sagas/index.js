a43c0f6e0d1a9d78e75f083631c62ec5b863aef1
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-12-28 13:06:40 +0100

    Better check before to execute saveDefaults settings saga

--- src/dashboard/sagas/index.js
+++ src/dashboard/sagas/index.js
@@ -4,18 +4,18 @@ import * as deps from '../deps';
 import pkgJson from '../../../package.json'; // Remove this line and enter the pkgJson.name manually.
 
 export function* saveDefaults(action) {
-  if (action.name === pkgJson.name) {
-    yield put(deps.actions.saveSettingsRequested({
-      someValue: 'This is a value saved in the database.',
-    }, {
-      name: pkgJson.name,
-      siteId: action.siteId, // This is optional when editing a site.
-    }));
-  }
+  yield put(deps.actions.saveSettingsRequested({
+    someValue: 'This is a value saved in the database.',
+  }, {
+    name: pkgJson.name,
+    siteId: action.siteId, // This is optional when editing a site.
+  }));
 }
 
 export default function* testSagas() {
   yield [
-    takeEvery(deps.types.DEFAULT_SETTINGS_NEEDED, saveDefaults),
+    takeEvery(action => action.type === deps.types.DEFAULT_SETTINGS_NEEDED
+      && action.name === pkgJson.name,
+      saveDefaults),
   ];
 }
