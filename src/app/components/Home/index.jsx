968947e73c816d1d04c752db9d557eaed20ceec0
Eduardo Campaña <asrielo@gmail.com>
2017-06-27 17:46:10 +0200

    Changed PostList scroll.
    Now it scrolls inside Home, and not below the header.

--- src/app/components/Home/index.jsx
+++ src/app/components/Home/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PostList from '../PostList';
 
-const Home = () => <PostList />;
+import styles from './styles.css';
+
+const Home = () =>
+  <div className={styles.home}>
+    <PostList />
+  </div>;
 
 export default Home;
