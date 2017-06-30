cd4a7cf98fe66108618badd64e0e57a9db2ba8d6
Eduardo Campaña <asrielo@gmail.com>
2017-06-30 10:39:43 +0200

    Changes font-weight in NavBar and fixes error on posts without media.

--- src/app/components/PostList/PostItem.jsx
+++ src/app/components/PostList/PostItem.jsx
@@ -6,22 +6,29 @@ import IconShare from 'react-icons/lib/md/share';
 import styles from './styles.css';
 
 const PostItem = ({ id, title, author, media, type }) => {
-  const images = media.media_details.sizes;
-  const alt = media.alt_text;
+  let alt;
+  let images;
+  let srcSet;
 
-  // Build srcset string for <img />
-  const srcSet = Object.keys(images)
-    .map(key => `${images[key].source_url} ${images[key].width}`)
-    .reduce((total, current) => `${total}${current}w, `, '');
+  if (media) {
+    alt = media.alt_text;
+    // Build srcset string for <img />
+    images = media.media_details.sizes;
+    srcSet = Object.keys(images)
+      .map(key => `${images[key].source_url} ${images[key].width}`)
+      .reduce((total, current) => `${total}${current}w, `, '');
+  }
 
   return (
     <div className={styles[`${type}Post`]}>
       <Link to={`?p=${id}`}>
-        <img className={styles[`${type}PostImage`]} alt={alt} srcSet={srcSet} />
+        {media && <img className={styles[`${type}PostImage`]} alt={alt} srcSet={srcSet} />}
         <div className={styles[`${type}PostInfo`]}>
           <p className={styles[`${type}PostTitle`]} dangerouslySetInnerHTML={{ __html: title }} />
           <Link to={`?author=${author.id}`}>
-            <p className={styles[`${type}PostAuthor`]}>{author.name}</p>
+            <p className={styles[`${type}PostAuthor`]}>
+              {author.name}
+            </p>
           </Link>
         </div>
       </Link>
@@ -35,7 +42,7 @@ const PostItem = ({ id, title, author, media, type }) => {
 PostItem.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  media: PropTypes.shape({}).isRequired,
+  media: PropTypes.shape({}),
   author: PropTypes.shape({}).isRequired,
   type: PropTypes.string.isRequired,
 };
