78104e8d93974d7f3dcfde0f6ce64aca9528464e
Eduardo Campaña <asrielo@gmail.com>
2017-07-12 12:17:37 +0200

    Closes #18 Links to post's categories and tags at the end of the post

--- src/app/components/Post/Footer.jsx
+++ src/app/components/Post/Footer.jsx
@@ -5,24 +5,20 @@ import styles from './styles.css';
 
 const Footer = ({ categories, tags }) =>
   <div className={styles.postFooter}>
-    <div>
-      {categories.map(category =>
-        <span key={category.id} className={styles.category}>
-          <Link to={`?cat=${category.id}`}>
-            {category.name}
-          </Link>
-        </span>
-      )}
-    </div>
-    <div>
-      {tags.map(tag =>
-        <span key={tag.id} className={styles.category}>
-          <Link to={`?tag=${tag.id}`}>
-            {tag.name}
-          </Link>
-        </span>
-      )}
-    </div>
+    {categories.map(category =>
+      <span key={category.id} className={styles.category}>
+        <Link to={`?cat=${category.id}`}>
+          {category.name}
+        </Link>
+      </span>
+    )}
+    {tags.map(tag =>
+      <span key={tag.id} className={styles.category}>
+        <Link to={`?tag=${tag.id}`}>
+          {tag.name}
+        </Link>
+      </span>
+    )}
   </div>;
 
 Footer.propTypes = {
