16c7036d76bf0360637ddf6aee4e6aa1e9dfbf42
David Arenas <darensr@gmail.com>
2017-07-13 12:05:51 +0200

    Moved sagas related to share modal in a separate file.

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,48 +1,8 @@
-import { takeEvery } from 'redux-saga';
-import { put, call, select } from 'redux-saga/effects';
-import * as countGetters from 'react-share/lib/share-count-getters';
-import * as actions from '../actions';
-import * as types from '../types';
-import * as deps from '../deps';
+import { fork } from 'redux-saga/effects';
+import shareModalSagas from './shareModal';
 
-const mapNetworkToGetter = {
-  facebook: countGetters.getFacebookShareCount,
-  linkedin: countGetters.getLinkedinShareCount,
-  google: countGetters.getGooglePlusShareCount,
-  pinterest: countGetters.getPinterestShareCount,
-  vk: countGetters.getVKShareCount,
-  ok: countGetters.getOKShareCount,
-  reddit: countGetters.getRedditShareCount,
-};
-
-const countPromise = (getter, url) =>
-  new Promise((resolve, reject) => {
-    getter(url, value => (typeof value === 'number' ? resolve({ value }) : reject({ value })));
-  });
-
-function* getSingleCount(network, entity) {
-  try {
-    const response = yield call(countPromise, mapNetworkToGetter[network], entity.link);
-    yield put(actions.shareModal.countSucceed({ id: entity.id, network, value: response.value }));
-  } catch (e) {
-    return;
-  }
-}
-
-function* shareCountsSaga(action) {
-  const entity = yield select(deps.selectorCreators.getWpTypeById(action.wpType, action.id));
+export default function* saturnSagas() {
   yield [
-    call(getSingleCount, 'facebook', entity),
-    call(getSingleCount, 'linkedin', entity),
-    call(getSingleCount, 'google', entity),
-    call(getSingleCount, 'pinterest', entity),
-    call(getSingleCount, 'vk', entity),
-    call(getSingleCount, 'ok', entity),
-    call(getSingleCount, 'reddit', entity),
+    fork(shareModalSagas),
   ];
-  yield put(actions.shareModal.allCountSucceed({ id: action.id }));
-}
-
-export default function* ivorySagas() {
-  yield takeEvery(types.ALL_SHARE_COUNT_REQUESTED, shareCountsSaga);
 }
---
+++ src/app/sagas/shareModal.js
@@ -0,0 +1,48 @@
+import { takeEvery } from 'redux-saga';
+import { put, call, select } from 'redux-saga/effects';
+import * as countGetters from 'react-share/lib/share-count-getters';
+import * as actions from '../actions';
+import * as types from '../types';
+import * as deps from '../deps';
+
+const mapNetworkToGetter = {
+  facebook: countGetters.getFacebookShareCount,
+  linkedin: countGetters.getLinkedinShareCount,
+  google: countGetters.getGooglePlusShareCount,
+  pinterest: countGetters.getPinterestShareCount,
+  vk: countGetters.getVKShareCount,
+  ok: countGetters.getOKShareCount,
+  reddit: countGetters.getRedditShareCount,
+};
+
+const countPromise = (getter, url) =>
+  new Promise((resolve, reject) => {
+    getter(url, value => (typeof value === 'number' ? resolve({ value }) : reject({ value })));
+  });
+
+function* getSingleCount(network, entity) {
+  try {
+    const response = yield call(countPromise, mapNetworkToGetter[network], entity.link);
+    yield put(actions.shareModal.countSucceed({ id: entity.id, network, value: response.value }));
+  } catch (e) {
+    return;
+  }
+}
+
+function* shareCountsSaga(action) {
+  const entity = yield select(deps.selectorCreators.getWpTypeById(action.wpType, action.id));
+  yield [
+    call(getSingleCount, 'facebook', entity),
+    call(getSingleCount, 'linkedin', entity),
+    call(getSingleCount, 'google', entity),
+    call(getSingleCount, 'pinterest', entity),
+    call(getSingleCount, 'vk', entity),
+    call(getSingleCount, 'ok', entity),
+    call(getSingleCount, 'reddit', entity),
+  ];
+  yield put(actions.shareModal.allCountSucceed({ id: action.id }));
+}
+
+export default function* shareModalSagas() {
+  yield takeEvery(types.ALL_SHARE_COUNT_REQUESTED, shareCountsSaga);
+}
