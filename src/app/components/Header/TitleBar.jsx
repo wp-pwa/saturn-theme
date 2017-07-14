8afb86c2d0865959c50eab9098d0a0d3c4088610
Eduardo Campaña <asrielo@gmail.com>
2017-07-14 13:26:47 +0200

    Refactors Header
    Now TitleBar and NavBar are independent from Header.
    
    NavBar animation has been simplified.

--- src/app/components/Header/TitleBar.jsx
+++ src/app/components/Header/TitleBar.jsx
@@ -1,4 +1,6 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { actions, selectors, selectorCreators } from '../../deps';
 import Menu from './Menu';
 import Logo from './Logo';
 import Slide from './Slide';
@@ -6,34 +8,62 @@ import CloseButton from './CloseButton';
 
 import styles from './styles.css';
 
-const TitleBar = ({
-  categories,
-  categoriesList,
-  currentCat,
-  currentTag,
-  currentAuthor,
-  currentPost,
-}) =>
-  <div className={`${styles.titleBar}`}>
-    <Menu
-      categories={categories}
-      categoriesList={categoriesList}
-      currentCat={currentCat}
-      currentTag={currentTag}
-      currentAuthor={currentAuthor}
-      currentPost={currentPost}
-    />
-    {currentPost ? <Slide /> : <Logo />}
-    {!!currentPost && <CloseButton />}
-  </div>;
+class TitleBar extends Component {
+  componentWillMount() {
+    this.props.getCategories();
+  }
+
+  render() {
+    const {
+      categories,
+      categoriesList,
+      currentCat,
+      currentTag,
+      currentAuthor,
+      currentPost,
+    } = this.props;
+
+    return (
+      <div className={`${styles.titleBar}`}>
+        <Menu
+          categories={categories}
+          categoriesList={categoriesList}
+          currentCat={currentCat}
+          currentTag={currentTag}
+          currentAuthor={currentAuthor}
+          currentPost={currentPost}
+        />
+        {currentPost ? <Slide /> : <Logo />}
+        {!!currentPost && <CloseButton />}
+      </div>
+    );
+  }
+}
 
 TitleBar.propTypes = {
   categories: PropTypes.shape({}).isRequired,
   categoriesList: PropTypes.arrayOf(PropTypes.number).isRequired,
-  currentCat: PropTypes.number,
-  currentTag: PropTypes.number,
-  currentAuthor: PropTypes.number,
-  currentPost: PropTypes.number,
+  currentCat: PropTypes.number.isRequired,
+  currentTag: PropTypes.number.isRequired,
+  currentAuthor: PropTypes.number.isRequired,
+  currentPost: PropTypes.number.isRequired,
+  getCategories: PropTypes.func.isRequired,
 };
 
-export default TitleBar;
+const mapStateToProps = state => ({
+  categories: selectors.getCategoriesEntities(state),
+  categoriesList: selectorCreators.getListResults('allCategories')(state),
+  currentCat: parseInt(selectors.getURLQueries(state).cat, 10) || 0,
+  currentTag: parseInt(selectors.getURLQueries(state).tag, 10) || 0,
+  currentAuthor: parseInt(selectors.getURLQueries(state).author, 10) || 0,
+  currentPost: parseInt(selectors.getURLQueries(state).p, 10) || 0,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getCategories: () =>
+    dispatch(
+      actions.newCategoriesListRequested({ name: 'allCategories', params: { per_page: 99 } })
+    ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TitleBar);
