9898f9514553e66b41f9cb9571d1072acde25786
David Arenas <darensr@gmail.com>
2017-07-17 14:57:34 +0200

    Changes in style of comments section.

--- src/app/components/Comments/index.jsx
+++ src/app/components/Comments/index.jsx
@@ -2,8 +2,8 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import ReactDisqusComments from 'react-disqus-comments';
 
-import CommentsIcon from 'react-icons/lib/go/comment-discussion';
-import ArrowDownIcon from 'react-icons/lib/fa/angle-down';
+import CommentsIcon from 'react-icons/lib/fa/comments-o';
+import ArrowIcon from 'react-icons/lib/fa/angle-down';
 
 import * as selectors from '../../selectors';
 import * as actions from '../../actions';
@@ -12,13 +12,13 @@ import * as deps from '../../deps';
 import styles from './styles.css';
 
 const Comments = ({ article, isOpen, toggle, disqusShortname }) =>
-  <div className={`${isOpen && styles.active}`}>
+  <div className={`${isOpen && styles.active} ${styles.commentsContainer}`}>
     <button className={styles.button} onClick={toggle}>
-      <CommentsIcon size={40} style={{ fill: 'white' }} />
+      <CommentsIcon className={styles.icon} size={40} />
       <div>
         {'Comentarios'}
       </div>
-      <ArrowDownIcon className={styles.icon} size={40} style={{ fill: 'black' }} />
+      <ArrowIcon className={`${styles.arrow} ${styles.icon}`} size={40} />
     </button>
     <div className={styles.comments}>
       <ReactDisqusComments
