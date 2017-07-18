55e7c673fcb5d75227f7a60a414c5f2a841b7d71
David Arenas <darensr@gmail.com>
2017-07-18 16:50:27 +0200

    Added AmpComponent.

--- src/app/components/Post/Content.jsx
+++ src/app/components/Post/Content.jsx
@@ -2,8 +2,25 @@
 import React, { PropTypes } from 'react';
 
 import HtmlToReactConverter from '../HtmlToReactConverter';
+import AmpComponent from '../AmpComponent';
 
 import styles from './styles.css';
+
+const imgToAmp = {
+  test: element => element.tagName === 'img',
+  converter: element => {
+    const converted = { ...element };
+    converted.tagName = AmpComponent;
+    converted.attributes = {
+      ...element.attributes,
+      ampTag: 'amp-img',
+      layout: 'responsive',
+      width: element.attributes.width || '16',
+      height: element.attributes.height || '9',
+    };
+    return converted;
+  },
+};
 
 const Content = ({ content }) =>
   <div className={styles.content}>
