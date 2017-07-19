c1806132dac9874f136d50b48e5186a71921481f
Eduardo Campaña <asrielo@gmail.com>
2017-07-19 19:10:25 +0200

    Adds animationTransition to Slider

--- src/app/components/Post/index.jsx
+++ src/app/components/Post/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import SwipeableViews from 'react-swipeable-views';
 import { virtualize } from 'react-swipeable-views-utils';
@@ -14,87 +14,76 @@ import styles from './styles.css';
 
 const Slider = virtualize(SwipeableViews);
 
-class Post extends Component {
-  componentWillMount() {
-    const { sliderLength, changeActiveSlide, postList, post } = this.props;
-
-    if (sliderLength) {
-      changeActiveSlide(postList.indexOf(post.id), null);
-    }
+const Post = ({
+  post,
+  posts,
+  postList,
+  isPostReady,
+  isListReady,
+  users,
+  categories,
+  tags,
+  sliderLength,
+  activeSlide,
+  changeActiveSlide,
+}) => {
+  if (!isPostReady) {
+    return (
+      <div className={styles.wrap}>
+        <Spinner />
+      </div>
+    );
   }
 
-  render() {
-    const {
-      post,
-      posts,
-      postList,
-      isPostReady,
-      isListReady,
-      users,
-      categories,
-      tags,
-      sliderLength,
-      activeSlide,
-    } = this.props;
-
-    if (!isPostReady) {
-      return (
-        <div className={styles.wrap}>
-          <Spinner />
-        </div>
-      );
-    }
-
-    if (!isListReady) {
-      return (
-        <div>
-          <SwipeableViews>
-            <PostItem post={post} users={users} categories={categories} tags={tags} active />
-          </SwipeableViews>
-          <ShareBar />
-        </div>
-      );
-    }
-
-    const sliderPosts = postList.map(id => posts[id]);
-
-    const slideRenderer = ({ key, index }) => {
-      let i = index;
-      if (index < 0) i = sliderLength + index;
-      else if (index > sliderLength - 1) i = index % sliderLength;
-
-      return (
-        <PostItem
-          key={key}
-          post={sliderPosts[i]}
-          users={users}
-          categories={categories}
-          tags={tags}
-          active={activeSlide === index}
-        />
-      );
-    };
-
+  if (!isListReady) {
     return (
       <div>
-        <Slider
-          index={activeSlide}
-          animateHeight
-          animateTransitions={false}
-          overscanSlideAfter={1}
-          overscanSlideBefore={1}
-          slideRenderer={slideRenderer}
-          onChangeIndex={(index, latestIndex) => {
-            const sliderAnimation = index > latestIndex ? 'right' : 'left';
-
-            this.props.changeActiveSlide(index, sliderAnimation);
-          }}
-        />
+        <SwipeableViews>
+          <PostItem post={post} users={users} categories={categories} tags={tags} active />
+        </SwipeableViews>
         <ShareBar />
       </div>
     );
   }
-}
+
+  const sliderPosts = postList.map(id => posts[id]);
+
+  //eslint-disable-next-line
+  const slideRenderer = ({ key, index }) => {
+    let i = index;
+    if (index < 0) i = sliderLength + index;
+    else if (index > sliderLength - 1) i = index % sliderLength;
+
+    return (
+      <PostItem
+        key={key}
+        post={sliderPosts[i]}
+        users={users}
+        categories={categories}
+        tags={tags}
+        active={activeSlide === index}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <Slider
+        index={activeSlide}
+        animateHeight
+        overscanSlideAfter={1}
+        overscanSlideBefore={1}
+        slideRenderer={slideRenderer}
+        onChangeIndex={(index, latestIndex) => {
+          const sliderAnimation = index > latestIndex ? 'right' : 'left';
+
+          changeActiveSlide(index, sliderAnimation);
+        }}
+      />
+      <ShareBar />
+    </div>
+  );
+};
 
 Post.propTypes = {
   post: PropTypes.shape({}),
