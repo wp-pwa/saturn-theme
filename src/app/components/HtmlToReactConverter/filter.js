cdc763ff03e00fb018acee77a79194e560776f3e
David Arenas <darensr@gmail.com>
2017-07-20 17:19:03 +0200

    Created filter function for html attributes. Fixes #44.

---
+++ src/app/components/HtmlToReactConverter/filter.js
@@ -0,0 +1,32 @@
+import htmlMap from './htmlMap';
+import svgMap from './svgMap';
+
+const allMap = { ...htmlMap, ...svgMap };
+
+const camelCaseToDash = str => str.replace(/([A-Z])/g, g => `-${g[0].toLowerCase()}`);
+
+export const replaceDataAttrs = dataset => {
+  const toReturn = {};
+  if (dataset) {
+    for (const [key, value] of Object.entries(dataset)) {
+      toReturn[`data-${camelCaseToDash(key)}`] = value;
+    }
+  }
+  return toReturn;
+};
+
+export const replaceAttrs = attributes => {
+  const toReturn = {};
+  if (attributes) {
+    for (const [key, value] of Object.entries(attributes)) {
+      const newKey = allMap[key.toLowerCase()];
+      toReturn[newKey && newKey !== key ? newKey : key] = value;
+    }
+  }
+  return toReturn;
+};
+
+export const filter = attributes => {
+  const { dataset, ...others } = attributes;
+  return { ...replaceDataAttrs(dataset), ...replaceAttrs(others) };
+};
