f9975136231bfe3202a182e99b373561b98d0a92
Pablo Postigo <pablo@worona.org>
2017-07-21 18:11:30 +0200

    Revert "WIP configurable menu in the dashboard"
    This reverts commit ee1b03cf70132498b08dd6836396a103f03e0f28.

--- src/dashboard/deps/index.js
+++ src/dashboard/deps/index.js
@@ -13,16 +13,12 @@ export const actions = {
 export const selectors = {
   get getSelectedSiteId() { return dep('router', 'selectors', 'getSelectedSiteId'); },
   get getSavingSettings() { return dep('settings', 'selectors', 'getSavingSettings'); },
-  get getSelectedSite() { return dep('sites', 'selectors', 'getSelectedSite'); },
 };
 
 export const selectorCreators = {
   get getSettings() { return dep('settings', 'selectorCreators', 'getSettings'); },
-  get getSetting() { return dep('settings', 'selectorCreators', 'getSetting'); },
 };
 
 export const types = {
   get DEFAULT_SETTINGS_NEEDED() { return dep('settings', 'types', 'DEFAULT_SETTINGS_NEEDED'); },
-  get SITE_SELECTED() { return dep('router', 'types', 'SITE_SELECTED'); },
-  get SITE_UNSELECTED() { return dep('router', 'types', 'SITE_UNSELECTED'); },
 };
--- src/dashboard/selectors/index.js
+++ src/dashboard/selectors/index.js
@@ -1,20 +1,3 @@
-import { createSelector } from 'reselect';
 import * as deps from '../deps';
 
-export const getMenuItemOpen = state => state.starterProTheme.menuItemOpen;
-export const getCurrentMenuItems = state => state.starterProTheme.currentMenuItems;
-export const getCategoriesList = state => state.starterProTheme.categoriesList;
-export const getPagesList = state => state.starterProTheme.pagesList;
 export const getThemeSettings = state => deps.selectorCreators.getSettings('saturnTheme')(state);
-export const getCategoriesStatus = state => state.starterProTheme.categoriesStatus;
-export const getPagesStatus = state => state.starterProTheme.pagesStatus;
-export const getStatus = createSelector(
-  getCategoriesStatus,
-  getPagesStatus,
-  (categories, pages) => {
-    if (categories === 'error' || pages === 'error') return 'error';
-    else if (categories === 'fetching' || pages === 'fetching') return 'fetching';
-    else if (categories === 'succeed' && pages === 'succeed') return 'succeed';
-    return 'idle';
-  }
-);
