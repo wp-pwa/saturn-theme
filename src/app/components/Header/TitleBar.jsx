11ffbbe73e36ea072bc433feeed6f249020a6e40
Pablo Postigo <pablo@worona.org>
2017-07-26 19:57:49 +0200

    WIP lateral menu. Items working properly, some styling to fix

--- src/app/components/Header/TitleBar.jsx
+++ src/app/components/Header/TitleBar.jsx
@@ -1,62 +1,44 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { actions, selectors, selectorCreators } from '../../deps';
+import { selectors, selectorCreators } from '../../deps';
 import * as libs from '../../libs';
 import Menu from './Menu';
 import Logo from './Logo';
 import Slide from './Slide';
 import CloseButton from './CloseButton';
-
 import styles from './styles.css';
 
-class TitleBar extends Component {
-  componentWillMount() {
-    this.props.getCategories();
-  }
+const TitleBar = ({ menuItemsList, currentCat,
+currentTag, currentAuthor, currentPost, mainColor }) => {
+  const bnColor = libs.blackOrWhite(mainColor);
 
-  render() {
-    const {
-      categories,
-      categoriesList,
-      currentCat,
-      currentTag,
-      currentAuthor,
-      currentPost,
-      mainColor,
-    } = this.props;
-    const bnColor = libs.blackOrWhite(mainColor);
+  return (
+    <div className={`${styles.titleBar}`} style={{ backgroundColor: mainColor, color: bnColor }} >
+      <Menu
+        menuItemsList={menuItemsList}
+        currentCat={currentCat}
+        currentTag={currentTag}
+        currentAuthor={currentAuthor}
+        currentPost={currentPost}
+      />
+      {currentPost ? <Slide /> : <Logo />}
+      {!!currentPost && <CloseButton />}
+    </div>
+  );
+};
 
-    return (
-      <div className={`${styles.titleBar}`} style={{ backgroundColor: mainColor, color: bnColor }} >
-        <Menu
-          categories={categories}
-          categoriesList={categoriesList}
-          currentCat={currentCat}
-          currentTag={currentTag}
-          currentAuthor={currentAuthor}
-          currentPost={currentPost}
-        />
-        {currentPost ? <Slide /> : <Logo />}
-        {!!currentPost && <CloseButton />}
-      </div>
-    );
-  }
-}
 
 TitleBar.propTypes = {
-  categories: PropTypes.shape({}).isRequired,
-  categoriesList: PropTypes.arrayOf(PropTypes.number).isRequired,
+  menuItemsList: PropTypes.arrayOf(PropTypes.object).isRequired,
   currentCat: PropTypes.number.isRequired,
   currentTag: PropTypes.number.isRequired,
   currentAuthor: PropTypes.number.isRequired,
   currentPost: PropTypes.number.isRequired,
-  getCategories: PropTypes.func.isRequired,
   mainColor: PropTypes.string,
 };
 
 const mapStateToProps = state => ({
-  categories: selectors.getCategoriesEntities(state),
-  categoriesList: selectorCreators.getListResults('allCategories')(state),
+  menuItemsList: selectorCreators.getSetting('theme', 'menu')(state),
   currentCat: parseInt(selectors.getURLQueries(state).cat, 10) || 0,
   currentTag: parseInt(selectors.getURLQueries(state).tag, 10) || 0,
   currentAuthor: parseInt(selectors.getURLQueries(state).author, 10) || 0,
@@ -64,11 +46,4 @@ const mapStateToProps = state => ({
   mainColor: selectorCreators.getSetting('theme', 'mainColor')(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  getCategories: () =>
-    dispatch(
-      actions.newCategoriesListRequested({ name: 'allCategories', params: { per_page: 99 } })
-    ),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(TitleBar);
+export default connect(mapStateToProps)(TitleBar);
