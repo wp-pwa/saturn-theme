964dbb164156d3c11de45e494ae7a1dd1c603c6c
David Arenas <darensr@gmail.com>
2017-08-02 16:49:25 +0200

    Ad component is now working in multiple posts

--- src/app/components/Ad/index.jsx
+++ src/app/components/Ad/index.jsx
@@ -1,81 +1,39 @@
 /* global sas */
 import React, { PropTypes } from 'react';
-// import { compose, lifecycle } from 'recompose';
 import LazyLoad from 'react-lazy-load';
+import uniqid from 'uniqid';
 
 import styles from './styles.css';
 
-class Ad extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.loadAd = this.loadAd.bind(this);
-  }
-
-  shouldComponentUpdate() {
-    return true;
-  }
-
-  loadAd() {
-    console.log('VISIBLE');
-    const { siteId, pageId, formatId, width, height, target } = this.props;
-    sas.call('iframe', {
-      siteId,
-      pageId,
-      formatId,
-      width,
-      height,
-      target,
-      async: true,
-      inSequence: true,
-    }, {
-      beforeRender: (data) => {
-        console.log('BEFORE RENDER', data);
-        setTimeout(() => {
-          console.log('ID REMOVED', data.formatId);
-          // this.adDiv.id = '';
-        });
-      },
-      onError: (data) => {
-        console.log('ON ERROR', data);
-      },
-      onLoad: (data) => {
-        console.log('ON LOAD', data);
-        // this.adDiv.id = '';
-      },
-    });
-  }
-
-  render() {
-    return (
-      <div className={styles.ad}>
-        <div
-          style={{
-            width: `${this.props.width}px`,
-            height: `${this.props.height}px`,
-            background: '#f5f5f5',
+const Ad = (props) => {
+  const { siteId, pageId, formatId, target, width, height } = props;
+  const tagId = `${formatId}_${uniqid.time()}`;
+  return (
+    <div className={styles.ad}>
+      <div
+        className={styles.container}
+        style={{ width: `${width}px`, height: `${height}px` }}
+      >
+        <LazyLoad
+          offsetHorizontal={0}
+          offsetVertical={400}
+          throttle={100}
+          width={width}
+          height={height}
+          onContentVisible={() => {
+            sas.call('iframe', {
+              siteId, pageId, formatId, target, width, height, tagId, async: true,
+            });
           }}
         >
-          {this.props.inActivePost && (
-            <LazyLoad
-              offsetHorizontal={-50}
-              offsetVertical={400}
-              throttle={100}
-              width={this.props.width}
-              height={this.props.height}
-              onContentVisible={this.loadAd}
-            >
-              <div id={`sas_${this.props.formatId}`} ref={adDiv => (this.adDiv = adDiv)} />
-            </LazyLoad>
-          )}
-        </div>
+          <div id={tagId} />
+        </LazyLoad>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Ad.propTypes = {
-  inActivePost: PropTypes.bool.isRequired,
   siteId: PropTypes.number.isRequired,
   pageId: PropTypes.number.isRequired,
   formatId: PropTypes.number.isRequired,
@@ -85,8 +43,3 @@ Ad.propTypes = {
 };
 
 export default Ad;
-
-// export default compose(
-//   lifecycle({
-//   })
-// )(Ad);
