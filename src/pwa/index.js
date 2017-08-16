a8373ecea0ff9b8f0148566c12ac84c177430596
Luis Herranz <luisherranz@gmail.com>
2017-08-16 13:29:20 +0200

    Export Theme as the default so HMR works.

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -1,10 +1,11 @@
 import { setStatic, compose } from 'recompose';
 import Theme from './components/Theme';
-// import reducers from './reducers';
-
-const Saturn = () => null;
+import * as actions from './actions';
+import * as types from './types';
+import reducers from './reducers';
 
 export default compose(
-  setStatic('Theme', Theme),
-  // setStatic('reducers', reducers),
-)(Saturn);
+  setStatic('actions', actions),
+  setStatic('types', types),
+  setStatic('reducers', reducers),
+)(Theme);
