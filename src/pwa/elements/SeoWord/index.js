7a2dce81060266c3488119a941db31c0f36b0753
Luis Herranz <luisherranz@gmail.com>
2017-10-06 15:26:26 +0200

    Fix SeoWord for SSR

--- src/pwa/elements/SeoWord/index.js
+++ src/pwa/elements/SeoWord/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { dep } from 'worona-deps';
 
-const SeoWord = () => {
-  if (window.location.href === 'https://demo.worona.org/the-beauties-of-gullfoss/')
-    return <span>viguhituworu</span>;
+const SeoWord = ({ isDemo, isPost }) => {
+  if (isDemo && isPost) return <span>viguhituworu</span>;
   return null;
 };
+SeoWord.propTypes = {
+  isDemo: PropTypes.bool.isRequired,
+  isPost: PropTypes.bool.isRequired,
+}
 
-export default SeoWord;
+const mapStateToProps = state => ({
+  isDemo:
+    dep('settings', 'selectorCreators', 'getSetting')('generalSite', 'url')(state) ===
+    'https://demo.worona.org/',
+  isPost: dep('router', 'selectors', 'getId')(state) === 60,
+});
+
+export default connect(mapStateToProps)(SeoWord);
