a12eb8450e30f1be8fc56f70ed5a1fc83f7b5654
Luis Herranz <luisherranz@gmail.com>
2017-11-02 12:24:59 +0100

    Export reducers and fix dependency

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './css/index.css';
-// export { default as reducers } from './reducers';
+
+import reducers from './reducers';
 // export { default as sagas } from './sagas/client';
 
 const Theme = () => <div className={styles.red}>hi from saturn</div>;
 
 export default Theme;
+export { reducers };
--- src/pwa/reducers/ssr.js
+++ src/pwa/reducers/ssr.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
 import { dep } from 'worona-deps';
 
-const ROUTE_CHANGE_SUCCEED = dep('router', 'types', 'ROUTE_CHANGE_SUCCEED');
-
 const firstId = (state = false, { type, query }) => {
+  const ROUTE_CHANGE_SUCCEED = dep('connection', 'types', 'ROUTE_CHANGE_SUCCEED');
   if (type === ROUTE_CHANGE_SUCCEED && state !== null) {
     if (state === false && query && query.p) return query.p;
     if (query && typeof query.p === 'undefined') return null;
