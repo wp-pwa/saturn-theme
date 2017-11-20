56a67e8ba7765964359dd1bdff7c351a0422c6bb
Luis Herranz <luisherranz@gmail.com>
2017-11-20 16:36:09 +0100

    Export real Theme

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import styles from './css/index.css';
-
+import Theme from './components/Theme';
 import reducers from './reducers';
 // export { default as sagas } from './sagas/client';
-
-const Theme = () => <div className={styles.red}>hi from saturn</div>;
 
 export default Theme;
 export { reducers };
