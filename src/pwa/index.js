96da54310ea8c28d1f2982459ed04220fadeb32f
Luis Herranz <luisherranz@gmail.com>
2017-11-20 16:36:09 +0100

    Revert "Export real Theme"
    This reverts commit 56a67e8ba7765964359dd1bdff7c351a0422c6bb.

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -1,6 +1,10 @@
-import Theme from './components/Theme';
+import React from 'react';
+import styles from './css/index.css';
+
 import reducers from './reducers';
 // export { default as sagas } from './sagas/client';
+
+const Theme = () => <div className={styles.red}>hi from saturn</div>;
 
 export default Theme;
 export { reducers };
