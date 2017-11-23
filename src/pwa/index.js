c948a05ae9d75077ca7c5721f3109b631538bc7e
Luis Herranz <luisherranz@gmail.com>
2017-11-23 12:13:03 +0100

    Mobx inject example

--- src/pwa/index.js
+++ src/pwa/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { inject } from 'mobx-react';
 import styles from './css/index.css';
 
 import reducers from './reducers';
 // export { default as sagas } from './sagas/client';
 
-const Theme = () => <div className={styles.red}>hi from saturn</div>;
+const Theme = ({ type, id }) => <div className={styles.red}>hi from saturn {type} {id}</div>;
 
-export default Theme;
+export default inject(stores => ({
+  type: stores.connection.selected.type,
+  id: stores.connection.selected.id,
+}))(Theme);
 export { reducers };
