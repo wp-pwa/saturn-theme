f6d0c1f8c97b6662823e9dea3548710684bf0230
Luis Herranz <luisherranz@gmail.com>
2017-12-14 12:02:27 +0100

    We home context for tag links

--- src/pwa/components/Post/TagItem.js
+++ src/pwa/components/Post/TagItem.js
@@ -3,18 +3,13 @@ import PropTypes from 'prop-types';
 import { inject } from 'mobx-react';
 import styled from 'react-emotion';
 import { dep } from 'worona-deps';
+import { compose } from 'recompose';
+import { connect } from 'react-redux';
+import { home } from '../../contexts';
 
-const context = selected => ({
-  items: [ selected ],
-  infinite: true,
-  options: {
-    bar: 'list',
-  },
-});
-
-const TagItem = ({ name, Link, selected }) => (
+const TagItem = ({ name, Link, selected, context }) => (
   <Container>
-    <Link selected={selected} context={context(selected)}>
+    <Link selected={selected} context={context}>
       <A>{name}</A>
     </Link>
   </Container>
@@ -24,13 +19,22 @@ TagItem.propTypes = {
   name: PropTypes.string.isRequired,
   Link: PropTypes.func.isRequired,
   selected: PropTypes.shape().isRequired,
+  context: PropTypes.shape().isRequired,
 };
 
-export default inject(({ connection }, { id, type }) => ({
-  name: connection.single[type][id].name,
+const mapStateToProps = state => ({
+  menu: dep('settings', 'selectorCreators', 'getSetting')('theme', 'menu')(state),
   Link: dep('connection', 'components', 'Link'),
-  selected: { listType: type, listId: id }
-}))(TagItem);
+});
+
+export default compose(
+  connect(mapStateToProps),
+  inject(({ connection }, { id, type, menu }) => ({
+    name: connection.single[type][id].name,
+    selected: { listType: type, listId: id },
+    context: home(menu),
+  })),
+)(TagItem);
 
 const Container = styled.span`
   box-sizing: border-box;
