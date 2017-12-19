5366d70d3c9362a7d4a3e57370ab925659f67ca6
Eduardo Campaña <asrielo@gmail.com>
2017-12-19 18:16:11 +0100

    Changes MenuItem

--- src/pwa/components/Menu/MenuItem.js
+++ src/pwa/components/Menu/MenuItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { dep } from 'worona-deps';
@@ -6,46 +6,42 @@ import styled from 'react-emotion';
 import * as contexts from '../../contexts';
 import * as actions from '../../actions';
 
-class MenuItem extends Component {
-  static propTypes = {
-    type: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    url: PropTypes.string,
-    active: PropTypes.bool.isRequired,
-    selected: PropTypes.shape({}),
-    context: PropTypes.shape({}),
-    Link: PropTypes.func.isRequired,
-    menuHasClosed: PropTypes.func.isRequired,
-  };
-
-  static defaultProps = {
-    url: null,
-    selected: null,
-    context: null,
-  };
-
-  render() {
-    const { type, selected, context, label, active, url, Link, menuHasClosed } = this.props;
-
-    if (type === 'link') {
-      return (
-        <Container onClick={menuHasClosed}>
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            {label}
-          </a>
-        </Container>
-      );
-    }
-
+const MenuItem = ({ type, selected, context, label, active, url, Link, menuHasClosed }) => {
+  if (type === 'link') {
     return (
-      <Container isActive={active}>
-        <Link selected={selected} context={context}>
-          <a>{label}</a>
-        </Link>
+      <Container onClick={menuHasClosed}>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          {label}
+        </a>
       </Container>
     );
   }
-}
+
+  return (
+    <Container isActive={active}>
+      <Link selected={selected} context={context}>
+        <a>{label}</a>
+      </Link>
+    </Container>
+  );
+};
+
+MenuItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  url: PropTypes.string,
+  active: PropTypes.bool.isRequired,
+  selected: PropTypes.shape({}),
+  context: PropTypes.shape({}),
+  Link: PropTypes.func.isRequired,
+  menuHasClosed: PropTypes.func.isRequired
+};
+
+MenuItem.defaultProps = {
+  url: null,
+  selected: null,
+  context: null
+};
 
 const mapStateToProps = (state, { id, type }) => {
   const selected = {};
@@ -63,12 +59,12 @@ const mapStateToProps = (state, { id, type }) => {
   return {
     selected,
     context: contexts.home(menu),
-    Link: dep('connection', 'components', 'Link'),
-  }
+    Link: dep('connection', 'components', 'Link')
+  };
 };
 
 const mapDispatchToProps = dispatch => ({
-  menuHasClosed: () => dispatch(actions.menu.hasClosed()),
+  menuHasClosed: () => dispatch(actions.menu.hasClosed())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MenuItem);
