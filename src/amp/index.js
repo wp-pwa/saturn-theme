56a6d8ec6c70d522476b0f626e45d4a60ffe8705
Luis Herranz <luisherranz@gmail.com>
2018-01-03 17:49:09 +0100

    Add amp support

---
+++ src/amp/index.js
@@ -0,0 +1,3 @@
+export { default } from '../pwa/components';
+export { default as reducers } from '../pwa/reducers';
+export { default as sagas } from '../pwa/sagas/client';
---
+++ src/amp/sagas/server.js
@@ -0,0 +1,1 @@
+export { default } from '../../pwa/sagas/server';
