abb9f6881b652bd44fd7c1d97039415089509acb
David Arenas <darensr@gmail.com>
2018-01-04 23:43:52 +0100

    AdSense waits for an ad before calling for the next one

--- src/pwa/elements/Ad/AdSense.js
+++ src/pwa/elements/Ad/AdSense.js
@@ -1,66 +1,36 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-// const cbk = mutationsList => {
-//   Array.from(mutationsList)
-//     .filter(m => m.type === 'childList')
-//     .forEach(({ target }) => {
-//       console.log(`Mutation ${target.id}`);
-//       const { width, height, contentWindow, contentDocument } = target.querySelector('iframe');
-//       const { google_ad_width, google_ad_height } = contentWindow;
-//       // debugger
-//       setTimeout(() => {debugger}, 100);
-//       contentWindow.document.addEventListener('DOMContentLoaded', e => {
-//         debugger;
-//         console.log('DOMContentLoaded', e);
-//       });
-//       if (width != google_ad_width || height != google_ad_height) {
-//         console.warn('Size is not the same!!!');
-//       }
-//       console.log(`WIDTH  - frame: ${width}, inside: ${google_ad_width}`);
-//       console.log(`HEIGHT - frame: ${height}, inside: ${google_ad_height}`);
-//     });
-// };
-//
-// const childChanges = { childList: true };
-//
-// const observer = new window.MutationObserver(cbk);
+const resolverMap = new Map();
+const observer = new window.MutationObserver(([{ target }]) => {
+  const { width, height, contentWindow } = target.querySelector('iframe');
+  const { document, google_ad_width, google_ad_height } = contentWindow;
+  if (width != google_ad_width || height != google_ad_height) // eslint-disable-line
+    console.warn('Size is not the same!!!', target);
 
-// TRICKY PART
-// let currentPush = Promise.resolve();
-// window.pushDelay = 500; // millis
-// const delayPush = () =>
-//   new Promise(resolve => {
-//     console.log('PUSH');
-//     try {
-//       window.adsbygoogle = window.adsbygoogle || [];
-//       window.adsbygoogle.push({});
-//     } catch (e) {
-//       console.warn(e);
-//     } finally {
-//       setTimeout(() => resolve(), window.pushDelay);
-//     }
-//   });
-// const awesomePush = () => {
-//   currentPush = currentPush.then(delayPush);
-// };
+  const listener = () => {
+    document.removeEventListener('DOMContentLoaded', listener);
+    resolverMap.get(target)();
+    resolverMap.delete(target);
+  };
+  document.addEventListener('DOMContentLoaded', listener);
+});
+
+let currentPush = Promise.resolve();
 
 class AdSense extends PureComponent {
   static propTypes = {
     client: PropTypes.string.isRequired,
     slot: PropTypes.string.isRequired,
-    format: PropTypes.string,
+    slide: PropTypes.number.isRequired,
     width: PropTypes.number,
-    height: PropTypes.number,
+    height: PropTypes.number
   };
 
   static defaultProps = {
-    format: '',
     width: 300,
-    height: 250,
+    height: 250
   };
-
-  static currentPush = Promise.resolve();
 
   constructor(props) {
     super(props);
@@ -71,33 +41,17 @@ class AdSense extends PureComponent {
 
   componentDidMount() {
     if (window) {
-      // observer.observe(this.node, childChanges);
-      // awesomePush();
       this.push();
+      observer.observe(this.node, { childList: true });
     }
   }
 
   deferPush() {
     return new Promise(resolve => {
-      console.log('PUSH');
-      let obs; // observer
-      debugger;
-      const cb = ([{ target }]) => {
-        const { contentWindow } = target.querySelector('iframe');
-        const listener = () => {
-          console.log(`DOMContentLoaded ${target.id}`);
-          // contentWindow.document.removeEventListener('DOMContentLoaded', listener)
-          // obs.disconnect();
-          resolve();
-        }
-        contentWindow.document.addEventListener('DOMContentLoaded', listener);
-
-      };
       try {
         window.adsbygoogle = window.adsbygoogle || [];
         window.adsbygoogle.push({});
-        obs = new window.MutationObserver(cb);
-        obs.observe(this.node, { childList: true });
+        resolverMap.set(this.node, resolve);
       } catch (e) {
         console.warn(e);
         resolve();
@@ -106,12 +60,11 @@ class AdSense extends PureComponent {
   }
 
   push() {
-    AdSense.currentPush = AdSense.currentPush.then(this.deferPush);
+    currentPush = currentPush.then(this.deferPush);
   }
 
   render() {
-    const { client, slot, format, width, height, slide } = this.props;
-    console.log('rendering ad', { slot, slide });
+    const { client, slot, width, height, slide } = this.props;
     return (
       <ins
         id={`slot: ${slot}, slide: ${slide}`}
@@ -121,7 +74,6 @@ class AdSense extends PureComponent {
         className="adsbygoogle"
         data-ad-client={client}
         data-ad-slot={slot}
-        // data-ad-format={format}
         style={{ display: 'inline-block', width: `${width}px`, height: `${height}px` }}
       />
     );
