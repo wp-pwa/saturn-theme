80c6547b1b704a11ce8a7ff3dbc99eb6475a7ca5
Luis Herranz <luisherranz@gmail.com>
2018-01-05 10:32:12 +0100

    WIP ask for additional lists on amp server

--- src/amp/sagas/server.js
+++ src/amp/sagas/server.js
@@ -1,1 +1,17 @@
 export { default } from '../../pwa/sagas/server';
+
+// import { fork, take, put, all } from 'redux-saga/effects';
+// import { dep } from 'worona-deps';
+// import pwaServerSagas, { waitForList } from '../../pwa/sagas/server';
+//
+// export default function* ampServerSagas({ selected }) {
+//   yield fork(pwaServerSagas, { selected });
+//   yield take(dep('build', 'actionTypes', 'SERVER_SAGAS_INITIALIZED'));
+//   const listRequested = dep('connection', 'actions', 'listRequested');
+//   yield put(listRequested({ listType: 'latest', listId: 'post' }));
+//   // ...
+//   yield all([
+//     waitForList({ listType: 'latest', listId: 'post' })
+//     // ...
+//   ])
+// }
