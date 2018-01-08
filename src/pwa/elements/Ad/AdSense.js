60f8662949d729729467d793c9af02aa5f30bf4a
David Arenas <darensr@gmail.com>
2018-01-08 16:10:28 +0100

    Do not wait when adding an ad

--- src/pwa/elements/Ad/AdSense.js
+++ src/pwa/elements/Ad/AdSense.js
@@ -1,35 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const resolverMap = new Map();
-const observer = new window.MutationObserver(([{ target }]) => {
-  const { contentWindow: content } = target.querySelector('iframe');
-  const {
-    document: frameDocument,
-    google_ad_width: googleWidth,
-    google_ad_height: googleHeight,
-  } = content;
-
-  if (!googleWidth || !googleHeight) {
-    console.warn('Size is undefined!!!', target);
-  }
-
-  const listener = () => {
-    if (frameDocument.readyState === 'interactive' || frameDocument.readyState === 'complete') {
-      console.log('INTERACTIVE');
-      frameDocument.removeEventListener('readystatechange', listener);
-      const resolver = resolverMap.get(target);
-      if (resolver) {
-        resolver();
-        resolverMap.delete(target);
-      }
-    }
-  };
-  frameDocument.addEventListener('readystatechange', listener);
-});
-
-let currentPush = Promise.resolve();
-
 class AdSense extends PureComponent {
   static propTypes = {
     client: PropTypes.string.isRequired,
@@ -43,53 +14,26 @@ class AdSense extends PureComponent {
     height: 250,
   };
 
-  constructor(props) {
-    super(props);
-
-    this.deferPush = this.deferPush.bind(this);
-    this.push = this.push.bind(this);
+  static push() {
+    try {
+      window.adsbygoogle = window.adsbygoogle || [];
+      window.adsbygoogle.push({});
+    } catch (e) {
+      console.warn(e);
+    }
   }
 
   componentDidMount() {
-    if (window) {
-      this.push();
-      observer.observe(this.node, { childList: true });
-    }
+    if (window) AdSense.push();
   }
 
   componentWillUnmount() {
-    console.log('UNMOUNT');
-    const resolver = resolverMap.get(this.node);
-    if (resolver) {
-      resolver();
-      resolverMap.delete(this.node);
-    }
-
     // Removes Google's handler for this ad
     const iframe = this.node.querySelector('iframe');
     if (iframe) {
-      console.log('REMOVE HANDLER');
       const { google_iframe_oncopy: { handlers } } = window;
       delete handlers[iframe.id];
     }
-  }
-
-  deferPush() {
-    return new Promise(resolve => {
-      try {
-        window.adsbygoogle = window.adsbygoogle || [];
-        window.adsbygoogle.push({});
-        resolverMap.set(this.node, resolve);
-      } catch (e) {
-        console.warn(e);
-        resolve();
-      }
-    });
-  }
-
-  push() {
-    console.log('PUSH');
-    currentPush = currentPush.then(this.deferPush);
   }
 
   render() {
