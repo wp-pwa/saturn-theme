1c4f3110f1c03fa1bccd0cfd806e5baf9094ecba
David Arenas <darensr@gmail.com>
2018-01-12 12:13:44 +0100

    MyR custom footer with hardcoded siteIds and ads

--- src/pwa/components/MyrFooter/index.js
+++ src/pwa/components/MyrFooter/index.js
@@ -7,48 +7,85 @@ import Legal from './Legal';
 import Powered from './Powered';
 import Ad from '../../elements/Ad';
 
-const MyRFooter = ({ bar }) => (
-  <Container bar={bar}>
-    <Ad
-      type="adsense"
-      client="ca-pub-2096136633140656"
-      slot="7307919591"
-      format="link"
-      height={250}
-    />
-    <Ad
-      type="adsense"
-      client="ca-pub-2096136633140656"
-      slot="2470007999"
-      width={320}
-      height={100}
-    />
-    <Ad
-      type="adsense"
-      client="ca-pub-2096136633140656"
-      slot="2030539195"
-      width={320}
-      height={250}
-    />
-    <About />
-    <Legal />
-    <Powered />
-    <Ad
-      type="adsense"
-      client="ca-pub-2096136633140656"
-      slot="5423474394"
-      width={320}
-      height={600}
-    />
-  </Container>
-);
+const client = 'ca-pub-2096136633140656';
+
+const customAds = {
+  // herencia.net
+  uTJtb3FaGNZcNiyCb: [
+    {
+      slot: '2376107909',
+      height: 280,
+      format: 'link',
+    },
+    {
+      slot: '5254564442',
+      width: 320,
+      height: 280,
+    },
+    {
+      slot: '7796967304',
+      height: 1200,
+    },
+  ],
+  // wpdirecto.com
+  x27yj7ZTsPjEngPPy: [
+    {
+      slot: '3932732157',
+      width: '94%',
+      height: 280,
+      format: 'link',
+    },
+    {
+      slot: '7844057003',
+      width: 320,
+      height: 280,
+    },
+    {
+      slot: '7796967304',
+      height: 1200,
+    },
+  ],
+};
+
+const MyRFooter = ({ bar, siteId }) => {
+  const [link, mediumRectangle, matchedContent] = customAds[siteId];
+  return (
+    <Container bar={bar}>
+      <Ad
+        type="adsense"
+        client={client}
+        slot={link.slot}
+        format={link.format}
+        height={link.height}
+      />
+      <Ad
+        type="adsense"
+        client={client}
+        slot={mediumRectangle.slot}
+        width={mediumRectangle.width}
+        height={mediumRectangle.height}
+      />
+      <About />
+      <Legal />
+      <Powered />
+      <Ad
+        type="adsense"
+        client={client}
+        slot={matchedContent.slot}
+        width={matchedContent.width}
+        height={matchedContent.height}
+      />
+    </Container>
+  );
+};
 
 MyRFooter.propTypes = {
-  bar: PropTypes.string.isRequired
+  bar: PropTypes.string.isRequired,
+  siteId: PropTypes.string.isRequired,
 };
 
 export default inject(({ connection }) => ({
-  bar: connection.context.options.bar
+  bar: connection.context.options.bar,
 }))(MyRFooter);
 
 const Container = styled.div`
