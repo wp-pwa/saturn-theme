768997020b0e439ff46a43d6c5044821a6f2d9da
Eduardo Campaña <asrielo@gmail.com>
2018-03-05 15:48:00 +0100

    Fixes removeContentAds converter.

--- src/shared/converters/removeContentAds.js
+++ src/shared/converters/removeContentAds.js
@@ -5,12 +5,6 @@ export default {
     element.attributes &&
     ((element.attributes.className && element.attributes.className.includes('inside-banner')) ||
       (element.attributes.id && element.attributes.id === 'inside-banner')) &&
-    element.children.find(
-      child =>
-        child.tagName === 'ins' &&
-        child.attributes &&
-        child.attributes.className &&
-        child.attributes.className.includes('adsbygoogle'),
-    ),
+    element.children.find(child => child.tagName === 'ins' || child.tagName === 'center'),
   converter: () => null,
 };
