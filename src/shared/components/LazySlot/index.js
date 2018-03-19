12803f47408979cfdf4355a2d8b9cae54aa308db
David Arenas <darensr@gmail.com>
2018-03-19 13:32:22 +0100

    Removed active from state

--- src/shared/components/LazySlot/index.js
+++ src/shared/components/LazySlot/index.js
@@ -2,33 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import LazyLoad from 'react-lazy-fastdom';
 import { connect } from 'react-redux';
-import { inject } from 'mobx-react';
 import { Slot } from 'react-slot-fill';
 import { dep } from 'worona-deps';
 
 class LazySlot extends Component {
   constructor(props) {
     super(props);
-    const { active, ssr } = props;
-    this.state = { active, ssr };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const active = this.state.active || nextProps.active;
-    this.setState({ active });
+    const { ssr } = props;
+    this.state = { ssr };
   }
 
   render() {
 
     const { name, className } = this.props;
-    // const { active, ssr } = this.state;
+    const { ssr } = this.state;
 
-    // if (!active) return <div className={className} style={{ visibility: 'hidden' }} />;
-
-    // if (ssr) return <Slot name={name} className={className}/>
+    if (ssr) return <Slot name={name} className={className}/>
 
     return (
-      <LazyLoad offsetHorizontal={-1} throttle={50} className={className}>
+      <LazyLoad offsetHorizontal={-50} throttle={50} className={className}>
         <Slot name={name} className={className}/>
       </LazyLoad>
     );
@@ -36,7 +28,6 @@ class LazySlot extends Component {
 };
 
 LazySlot.propTypes = {
-  active: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
   ssr: PropTypes.bool.isRequired,
   className: PropTypes.string,
@@ -50,8 +41,4 @@ const mapStateToProps = state => ({
   ssr: dep('build', 'selectors', 'getSsr')(state),
 });
 
-export default connect(mapStateToProps)(
-  inject(({ connection: { selected } }, { type, id }) => ({
-    active: selected.type === type && selected.id === id,
-  }))(LazySlot),
-);
+export default connect(mapStateToProps)(LazySlot);
