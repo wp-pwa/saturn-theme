24dbcb9be79f633d10ce445b2962ed9f46a1a2a9
David Arenas <darensr@gmail.com>
2018-03-19 17:54:45 +0100

    Adds logic to LazySlot

--- src/shared/components/LazySlot/index.js
+++ src/shared/components/LazySlot/index.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import LazyLoad from 'react-lazy-fastdom';
+import styled from 'react-emotion';
 import { connect } from 'react-redux';
 import { Slot } from 'react-slot-fill';
 import { dep } from 'worona-deps';
+import Spinner from '../../../pwa/elements/Spinner';
 
 class LazySlot extends Component {
   constructor(props) {
     super(props);
     const { ssr } = props;
-    this.state = { ssr };
+    this.state = { ssr, loaded: ssr };
+    this.onLoad = this.onLoad.bind(this);
+  }
+
+  onLoad() {
+    this.setState({ loaded: true });
   }
 
   render() {
-
     const { name, className } = this.props;
-    const { ssr } = this.state;
+    const { ssr, loaded } = this.state;
 
-    if (ssr) return <Slot name={name} className={className}/>
+    if (ssr) return <Slot name={name} className={className} />;
 
     return (
-      <LazyLoad offsetHorizontal={-50} throttle={50} debounce={false} className={className}>
-        <Slot name={name} className={className}/>
-      </LazyLoad>
+      <Slot name={name} className={className}>
+        {([element]) =>
+          !ssr ? (
+            <Container className={className}>
+              <StyledLazy
+                offsetHorizontal={-50}
+                throttle={50}
+                loaded={loaded}
+                debounce={false}
+                className={className}>
+                {React.cloneElement(element, { onLoad: this.onLoad })}
+              </StyledLazy>
+              {!loaded && (
+                <SpinnerContainer>
+                  <Spinner />
+                </SpinnerContainer>
+              )}
+            </Container>
+          ) : (
+            element
+          )
+        }
+      </Slot>
     );
   }
-};
+}
 
 LazySlot.propTypes = {
   name: PropTypes.string.isRequired,
@@ -42,3 +68,24 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(LazySlot);
+
+const StyledLazy = styled(LazyLoad)`
+  filter: ${({ loaded }) => (loaded ? 'opacity(100%)' : 'opacity(0)')};
+  transition: filter 300ms ease-in;
+`;
+
+const Container = styled.div`
+  display: block;
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+
+const SpinnerContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+`;
