98800a3dc58dd4072cfd55eb70fa75a379843239
Eduardo Campaña <asrielo@gmail.com>
2018-03-26 18:10:47 +0200

    Changes waitFor functions #197

--- src/pwa/sagas/server.js
+++ src/pwa/sagas/server.js
@@ -2,42 +2,38 @@ import { race, take, put, select } from 'redux-saga/effects';
 import { dep } from 'worona-deps';
 import { home, single } from '../contexts';
 
-export function* waitForList({ listType, listId, page }) {
+export function* waitForList({ type, id, page }) {
   const LIST_SUCCEED = dep('connection', 'actionTypes', 'LIST_SUCCEED');
   const LIST_FAILED = dep('connection', 'actionTypes', 'LIST_FAILED');
   yield race({
     succeed: take(
       action =>
         action.type === LIST_SUCCEED &&
-        action.listType === listType &&
-        action.listId === listId &&
-        action.page === page,
+        action.list.type === type &&
+        action.list.id === id &&
+        action.list.page === page,
     ),
     failed: take(
       action =>
         action.type === LIST_FAILED &&
-        action.listType === listType &&
-        action.listId === listId &&
+        action.list.type === type &&
+        action.list.id === id &&
         action.page === page,
     ),
   });
 }
 
-export function* waitForSingle({ singleType, singleId }) {
-  const SINGLE_SUCCEED = dep('connection', 'actionTypes', 'SINGLE_SUCCEED');
-  const SINGLE_FAILED = dep('connection', 'actionTypes', 'SINGLE_FAILED');
+export function* waitForEntity({ type, id }) {
+  const ENTITY_SUCCEED = dep('connection', 'actionTypes', 'ENTITY_SUCCEED');
+  const ENTITY_FAILED = dep('connection', 'actionTypes', 'ENTITY_FAILED');
   yield race({
     succeed: take(
       action =>
-        action.type === SINGLE_SUCCEED &&
-        action.singleType === singleType &&
-        action.singleId === singleId,
+        action.type === ENTITY_SUCCEED && action.entity.type === type && action.entity.id === id,
     ),
     failed: take(
       action =>
-        action.type === SINGLE_FAILED &&
-        action.singleType === singleType &&
-        action.singleId === singleId,
+        action.type === ENTITY_FAILED && action.entity.type === type && action.entity.id === id,
     ),
   });
 }
@@ -47,35 +43,36 @@ export function* waitForCustom({ name, page }) {
   const CUSTOM_FAILED = dep('connection', 'actionTypes', 'CUSTOM_FAILED');
   yield race({
     succeed: take(
-      action => action.type === CUSTOM_SUCCEED && action.name === name && action.page === page,
+      action =>
+        action.type === CUSTOM_SUCCEED &&
+        action.custom.name === name &&
+        action.custom.page === page,
     ),
     failed: take(
-      action => action.type === CUSTOM_FAILED && action.name === name && action.page === page,
+      action =>
+        action.type === CUSTOM_FAILED && action.custom.name === name && action.custom.page === page,
     ),
   });
 }
 
-export default function* saturnServerSaga(something) {
+export default function* saturnServerSaga({ selectedItem }) {
   yield take(dep('build', 'actionTypes', 'SERVER_SAGAS_INITIALIZED'));
   const routeChangeSucceed = dep('connection', 'actions', 'routeChangeSucceed');
   const routeChangeRequested = dep('connection', 'actions', 'routeChangeRequested');
-  console.log('something:', something);
-  const { isList } = selectedItem;
+  const { type, id, page } = selectedItem;
 
-  if (isList) {
+  if (typeof page !== 'undefined') {
     const menu = yield select(dep('settings', 'selectorCreators', 'getSetting')('theme', 'menu'));
     const context = home(menu);
-    const { type, id, page } = selectedItem;
     const action = { selectedItem: { type, id, page }, context };
     yield put(routeChangeRequested(action));
     yield put(routeChangeSucceed(action));
     yield waitForList({ type, id, page });
   } else {
     const context = single();
-    const { type, id } = selectedItem;
     const action = { selectedItem: { type, id }, context };
     yield put(routeChangeRequested(action));
     yield put(routeChangeSucceed(action));
-    yield waitForSingle({ type, id });
+    yield waitForEntity({ type, id });
   }
 }
