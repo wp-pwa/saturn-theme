08fc7992bfb14ca10e376e61e035bda0d848412f
Luis Herranz <luisherranz@gmail.com>
2018-04-26 12:35:34 +0200

    Remove memoize from selectors

--- src/pwa/selectorCreators/slots.js
+++ src/pwa/selectorCreators/slots.js
@@ -1,20 +1,18 @@
-import { memoize, isMatch } from 'lodash';
+import { isMatch } from 'lodash';
 import { getSlots } from '../selectors/slots';
 
 const reverseOrder = array => array.sort((a, b) => b.position - a.position);
 
-export const getSlotsForItem = memoize((type, id, page, state) =>
+export const getSlotsForItem = (type, id, page, state) =>
   reverseOrder(
     getSlots(state).filter(
       ({ rules }) => !!rules.item && rules.item.some(rule => isMatch({ type, id, page }, rule)),
     ),
-  ),
-);
+  );
 
-export const getSlotsForColumn = memoize((type, index, state) =>
+export const getSlotsForColumn = (type, index, state) =>
   reverseOrder(
     getSlots(state).filter(
       ({ rules }) => !!rules.column && rules.column.some(rule => isMatch({ type, index }, rule)),
     ),
-  ),
-);
+  );
