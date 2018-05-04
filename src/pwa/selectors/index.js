d4044e5a7a07381bed4e436a30ea9b11921518e2
Eduardo Campaña <asrielo@gmail.com>
2018-05-04 18:11:24 +0200

    Removes cookies selectors
    #275

--- src/pwa/selectors/index.js
+++ src/pwa/selectors/index.js
@@ -1,8 +1,7 @@
 import * as list from './list';
 import * as share from './share';
 import * as comments from './comments';
-import * as cookies from './cookies';
 import * as ads from './ads';
 import * as notifications from './notifications';
 
-export { list, share, comments, cookies, ads, notifications };
+export { list, share, comments, ads, notifications };
