7b960d74c930b1f7f2d863bd385780c2d11944f9
Eduardo Campaña <asrielo@gmail.com>
2018-05-07 08:32:34 +0200

    Removes notifications reducers and selectors
    #278

--- src/pwa/reducers/index.js
+++ src/pwa/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import share from './share';
-import notifications from './notifications';
 import scroll from './scroll';
 import sticky from './sticky';
 
 export default () =>
   combineReducers({
     share,
-    notifications,
     scroll,
     sticky,
   });
--- src/pwa/selectors/index.js
+++ src/pwa/selectors/index.js
@@ -2,6 +2,5 @@ import * as list from './list';
 import * as share from './share';
 import * as comments from './comments';
 import * as ads from './ads';
-import * as notifications from './notifications';
 
-export { list, share, comments, ads, notifications };
+export { list, share, comments, ads };
