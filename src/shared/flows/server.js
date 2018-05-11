9b85882185bce399d7beb0d5a66729e072980dd2
Eduardo Campaña <asrielo@gmail.com>
2018-05-11 12:46:28 +0200

    Removes console.log

--- src/shared/flows/server.js
+++ src/shared/flows/server.js
@@ -3,6 +3,5 @@ import { flow } from 'mobx-state-tree';
 export default self =>
   flow(function* ServerSaturn() {
     // Set the theme language.
-    console.log('something');
     self.theme.lang.setLang(self.settings.theme.lang);
   });
