f49b6318358018d4fbf59b47c99fd27fd5b45c63
Luis Herranz <luisherranz@gmail.com>
2018-05-16 16:45:28 +0200

    Add flows

--- src/pwa/client.js
+++ src/pwa/client.js
@@ -1,8 +1,6 @@
 import components from './components';
-import reducers from './reducers';
-import clientSagas from './sagas/client';
 import Store from '../shared/stores';
 import { version } from '../../package.json';
 
 export default components;
-export { reducers, clientSagas, Store, version };
+export { Store, version };
---
+++ src/pwa/server.js
@@ -0,0 +1,7 @@
+import components from './components';
+import Store from '../shared/stores';
+import flow from './flows/server';
+import { version } from '../../package.json';
+
+export default components;
+export { Store, version, flow };
