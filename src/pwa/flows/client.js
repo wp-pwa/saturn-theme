80bd6064c765ca3765fbb45db7c45d2a68006fc2
David Arenas <darensr@gmail.com>
2018-06-18 16:56:58 +0200

    Flow for requesting next column entities

--- src/pwa/flows/client.js
+++ src/pwa/flows/client.js
@@ -1,36 +1,18 @@
 import { flow, addMiddleware } from 'mobx-state-tree';
-import requestsMiddleware, { filterAlreadyRequested } from './requests';
+import { requestNextColumnEntities } from './requests';
+import { syncActionEnds } from './actionEnds';
 
 export default self =>
   flow(function* SaturnClientFlow() {
     const { connection } = self;
 
-    addMiddleware(connection, requestsMiddleware);
+    addMiddleware(
+      connection,
+      syncActionEnds('routeChangeSucceed', () => requestNextColumnEntities(connection)),
+    );
 
     // Handles intial requests in List view.
-    if (connection.selectedContext.options.bar === 'list') {
-      const { rawColumns } = connection.selectedContext;
-      const selectedColumnIndex = connection.selectedColumn.index;
-      const previousIndex = selectedColumnIndex === 0 ? null : selectedColumnIndex - 1;
-      const nextIndex =
-        selectedColumnIndex === rawColumns.length - 1 ? null : selectedColumnIndex + 1;
-
-      const neededColumns = [];
-
-      if (previousIndex !== null) neededColumns.push(rawColumns[previousIndex]);
-      if (nextIndex !== null) neededColumns.push(rawColumns[nextIndex]);
-
-      const items = neededColumns.map(column => ({
-        type: column.rawItems[0].type,
-        id: column.rawItems[0].id,
-        page: column.rawItems[0].page,
-      }));
-
-      filterAlreadyRequested(items, connection).forEach(item => {
-        if (item.page) connection.fetchListPage(item);
-        else connection.fetchEntity(item);
-      });
-    }
+    requestNextColumnEntities(connection);
 
     // Request next pages.
     yield self.theme.requestFirstExtracted();
