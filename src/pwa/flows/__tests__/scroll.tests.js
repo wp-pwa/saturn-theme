1244f9f1fa5b4506f9f5e55d636dfceccffed16c
Eduardo Campaña <asrielo@gmail.com>
2018-06-26 18:11:30 +0200

    Adds tests for scroll flow
    https://github.com/wp-pwa/saturn-theme/issues/48

--- src/pwa/flows/__tests__/scroll.tests.js
+++ src/pwa/flows/__tests__/scroll.tests.js
@@ -1,18 +1,69 @@
 import { types } from 'mobx-state-tree';
-import { initializeScrollListener } from '../scroll';
+import {
+  initializeScrollListener,
+  handleScroll,
+  scrollMiddleware,
+} from '../scroll';
 
 describe('Theme › Flows › PWA › Client › Scroll', () => {
-  const fastdomPromised = jest.fn();
-  const addEventListener = jest.fn();
+  const fastdomPromised = {
+    measure: jest.fn().mockReturnValue({ top: -20 }),
+  };
 
-  const self = types
-    .model('Scroll')
-    .actions(() => ({ hideBar: jest.fn(), showBar: jest.fn() }))
-    .create({}, { theme: { fastdomPromised, addEventListener } });
+  const self = types.model('Scroll').create({}, { theme: { fastdomPromised } });
+  self.handleScroll = jest.fn();
+  self.setRouteChanged = jest.fn();
 
   test('Listener is initialized', () => {
-    initializeScrollListener(self);
+    const listener = jest.spyOn(window, 'addEventListener');
 
-    expect(addEventListener).toHaveBeenCalled();
+    initializeScrollListener();
+
+    expect(listener).toHaveBeenCalled();
+  });
+
+  test('handleScroll is called with the right value', async () => {
+    await handleScroll(self);
+
+    expect(fastdomPromised.measure).toHaveBeenCalled();
+    expect(self.handleScroll).toHaveBeenCalledWith(-20);
+  });
+
+  test('scrollMiddleware is triggered', () => {
+    const next = jest.fn();
+    const call = {
+      name: 'routeChangeSucceed',
+      tree: {
+        theme: {
+          scroll: {
+            setRouteChanged: jest.fn(),
+          },
+        },
+      },
+    };
+
+    scrollMiddleware(call, next);
+
+    expect(next).toHaveBeenCalledWith(call);
+    expect(call.tree.theme.scroll.setRouteChanged).toHaveBeenCalledWith(true);
+  });
+
+  test('scrollMiddleware is not triggered', () => {
+    const next = jest.fn();
+    const call = {
+      name: 'NOT_routeChangeSucceed',
+      tree: {
+        theme: {
+          scroll: {
+            setRouteChanged: jest.fn(),
+          },
+        },
+      },
+    };
+
+    scrollMiddleware(call, next);
+
+    expect(next).toHaveBeenCalledWith(call);
+    expect(call.tree.theme.scroll.setRouteChanged).not.toHaveBeenCalled();
   });
 });
--- src/pwa/flows/scroll.js
+++ src/pwa/flows/scroll.js
@@ -1,19 +1,18 @@
 import { throttle } from 'lodash';
 import { getEnv } from 'mobx-state-tree';
 
-export const handleScroll = self =>
-  throttle(async () => {
-    const { fastdomPromised } = getEnv(self).theme;
+export const handleScroll = async self => {
+  const { fastdomPromised } = getEnv(self).theme;
 
-    const { top } = await fastdomPromised.measure(() =>
-      window.document.body.getBoundingClientRect(),
-    );
+  const { top } = await fastdomPromised.measure(() =>
+    window.document.body.getBoundingClientRect(),
+  );
 
-    self.handleScroll(top);
-  }, 100);
+  self.handleScroll(top);
+};
 
 export const initializeScrollListener = async self => {
-  window.addEventListener('scroll', handleScroll(self));
+  window.addEventListener('scroll', throttle(() => handleScroll(self), 100));
 };
 
 export const scrollMiddleware = (call, next) => {
