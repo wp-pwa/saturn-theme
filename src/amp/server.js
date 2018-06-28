ddb4bf65b41ba8c42602ba04adce9d2bd7599978
Eduardo Campaña <asrielo@gmail.com>
2018-06-28 11:43:35 +0200

    Fixes AMP entry point
    https://github.com/wp-pwa/saturn-theme/issues/318

--- src/amp/server.js
+++ src/amp/server.js
@@ -1,8 +1,7 @@
 import components from './components';
-import reducers from '../pwa/reducers';
 import * as analytics from './analytics';
 
 import Store from '../shared/stores';
 
 export default components;
-export { reducers, analytics, Store };
+export { analytics, Store };
