ed668a6e424b005a081cfe0b6e591994904bf41f
Eduardo Campaña <asrielo@gmail.com>
2018-07-06 12:50:56 +0200

    Centers logo in menu header
    https://github.com/wp-pwa/saturn-theme/issues/332

--- src/pwa/components/Menu/MenuHeader.js
+++ src/pwa/components/Menu/MenuHeader.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MenuLogo from './MenuLogo';
-// import CloseButton from './CloseButton';
 import { Container } from '../../../shared/styled/Menu/MenuHeader';
 
 const MenuHeader = () => (
   <Container>
     <MenuLogo />
-    {/* <CloseButton /> */}
   </Container>
 );
 
