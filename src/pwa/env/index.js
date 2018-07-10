a0641e436fd175044f87ea1319d3e0c5efdcc1d8
Eduardo Campaña <asrielo@gmail.com>
2018-07-10 19:21:37 +0200

    refactor(stores): adds a delay function to env to use in setLinkCopied

--- src/pwa/env/index.js
+++ src/pwa/env/index.js
@@ -3,5 +3,6 @@ import fastdom from 'fastdom';
 import fdPromised from 'fastdom/extensions/fastdom-promised';
 
 const fastdomPromised = fastdom.extend(fdPromised);
+const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
-export { fastdomPromised };
+export { fastdomPromised, delay };
