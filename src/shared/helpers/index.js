042b94862daaca15e6b12b18f1147b655a9ef46a
Luis Herranz <luisherranz@gmail.com>
2018-07-18 10:13:48 +0200

    fix(menu): refactor styles and add menuHeader to theme

--- src/shared/helpers/index.js
+++ src/shared/helpers/index.js
@@ -86,6 +86,7 @@ export const getThemeProps = color => ({
   heights: {
     bar: '54px',
     navbar: '30px',
+    menuHeader: '100px',
   },
   shadows: {
     top: '0 1px 3px rgba(0,0,0,0.10), 0 1px 2px rgba(0,0,0,0.15)',
