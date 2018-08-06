65db5f630840f95d3c9dfbf891e401facb74c611
Eduardo Campaña <asrielo@gmail.com>
2018-08-06 15:53:10 +0200

    feat(content): adds support for soundcloud
    fixes https://github.com/frontity/saturn-theme/issues/62

---
+++ src/shared/converters/soundcloud.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import LazySoundcloud from '../components/LazySoundcloud';
+import { filter } from '../components/HtmlToReactConverter/filter';
+
+export default {
+  test: ({ tagName, attributes }) =>
+    tagName === 'iframe' && attributes.src.includes('soundcloud'),
+  converter: element => {
+    const { attributes } = element;
+
+    let height;
+
+    if (attributes.height && attributes.width) {
+      if (attributes.width.includes('%')) {
+        height = `${attributes.height}px`;
+      } else {
+        height = `${(attributes.height * 100) / attributes.width}vw`; // prettier-ignore
+      }
+    } else {
+      height = '120px';
+    }
+
+    const httpRegexp = /^http:\/\//;
+
+    if (attributes.src.match(httpRegexp)) {
+      attributes.src = attributes.src.replace(httpRegexp, 'https://');
+    }
+
+    const [, track, color] = /tracks\/(\d+).+color=%(\w{8})/g.exec(
+      attributes.src,
+    );
+
+    return (
+      <LazySoundcloud
+        height={height}
+        width="100%"
+        track={track}
+        color={color}
+        attributes={filter(attributes)}
+      />
+    );
+  },
+};
