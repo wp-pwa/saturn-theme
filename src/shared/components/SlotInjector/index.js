26977f2b1f44babfddb3b322c4ed041601bf00e5
Luis Herranz <luisherranz@gmail.com>
2018-08-13 13:40:21 +0200

    chore(slots): add proper debug mode

--- src/shared/components/SlotInjector/index.js
+++ src/shared/components/SlotInjector/index.js
@@ -3,41 +3,45 @@ import PropTypes from 'prop-types';
 import { inject } from 'mobx-react';
 import { isMatch } from 'lodash';
 import { Slot } from 'react-slot-fill';
+import styled from 'react-emotion';
 
-const SlotInjector = ({ slots, position, item, ...fillChildProps }) => (
-  // // DEBUG AND TESTING
-  // <SlotMock>
-  //   {position}
-  //   {': '}
-  //   <Span>
-  //     {slots
-  //       .filter(
-  //         slot =>
-  //           slot.position === position &&
-  //           slot.items.some(i => isMatch(item, i)),
-  //       )
-  //       // keep in mind fillChildProps={fillChildProps}
-  //       .map(({ names }) => names.join(', ')).join(', ')}
-  //   </Span>
-  // </SlotMock>
+const SlotInjector = ({ slots, position, item, debug, ...fillChildProps }) => (
   <Fragment>
-    {slots
-      .filter(
-        slot =>
-          slot.position === position &&
-          slot.rules &&
-          slot.rules.item &&
-          slot.rules.item.some(i => isMatch(item, i)),
-      )
-      .map(({ names }) =>
-        names.map(name => (
-          <Slot
-            key={name}
-            name={name}
-            fillChildProps={{ item, ...fillChildProps }}
-          />
-        )),
-      )}
+    {debug ? (
+      <SlotMock>
+        {position}
+        {': '}
+        <Span>
+          {slots
+            .filter(
+              slot =>
+                slot.position === position &&
+                slot.items.some(i => isMatch(item, i)),
+            )
+            // keep in mind fillChildProps={fillChildProps}
+            .map(({ names }) => names.join(', '))
+            .join(', ')}
+        </Span>
+      </SlotMock>
+    ) : (
+      slots
+        .filter(
+          slot =>
+            slot.position === position &&
+            slot.rules &&
+            slot.rules.item &&
+            slot.rules.item.some(i => isMatch(item, i)),
+        )
+        .map(({ names }) =>
+          names.map(name => (
+            <Slot
+              key={name}
+              name={name}
+              fillChildProps={{ item, ...fillChildProps }}
+            />
+          )),
+        )
+    )}
   </Fragment>
 );
 
@@ -46,6 +50,7 @@ export default inject(({ stores: { settings } }) => ({
 }))(SlotInjector);
 
 SlotInjector.propTypes = {
+  debug: PropTypes.bool,
   slots: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   position: PropTypes.string.isRequired,
   item: PropTypes.shape({
@@ -56,16 +61,17 @@ SlotInjector.propTypes = {
 };
 
 SlotInjector.defaultProps = {
+  debug: true,
   item: {},
 };
 
-// const SlotMock = styled.div`
-//   font-size: 20px;
-//   background: blue;
-//   color: yellow;
-// `;
+const SlotMock = styled.div`
+  font-size: 20px;
+  background: blue;
+  color: yellow;
+`;
 
-// const Span = styled.span`
-//   font-weight: bold;
-//   color: white;
-// `;
+const Span = styled.span`
+  font-weight: bold;
+  color: white;
+`;
