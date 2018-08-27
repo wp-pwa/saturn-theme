b5f39fdb45274c1d5754beff2b9fb65e370b3440
Eduardo Campaña <asrielo@gmail.com>
2018-08-27 16:49:29 +0200

    refactor(menu): refactors menu to improve animation
    fixes https://github.com/frontity/saturn-theme/issues/109

--- src/pwa/components/Menu/index.js
+++ src/pwa/components/Menu/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { inject } from 'mobx-react';
 import styled from 'react-emotion';
@@ -6,13 +6,13 @@ import MenuHeader from './MenuHeader';
 import MenuList from './MenuList';
 
 const Menu = ({ isOpen, close }) => (
-  <Container isOpen={isOpen}>
+  <Fragment>
     <Overlay isOpen={isOpen} onClick={close} onTouchMove={close} />
-    <InnerContainer isOpen={isOpen}>
+    <Container isOpen={isOpen}>
       <MenuHeader />
       <MenuList />
-    </InnerContainer>
-  </Container>
+    </Container>
+  </Fragment>
 );
 
 Menu.propTypes = {
@@ -25,34 +25,28 @@ export default inject(({ stores: { theme } }) => ({
   close: theme.menu.close,
 }))(Menu);
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  visibility: ${({ isOpen }) => (isOpen ? 'visible' : 'hidden')};
-  transition: ${({ isOpen }) => (isOpen ? '' : 'visibility 0s ease-in 300ms')};
-  z-index: 150;
-`;
-
 const Overlay = styled.div`
-  filter: ${({ isOpen }) => (isOpen ? 'opacity(100%)' : 'opacity(0%)')};
-  transition: filter 300ms ease;
-  background-color: rgba(0, 0, 0, 0.5);
-  width: 100%;
+  position: fixed;
+  width: 100vw;
   height: 100%;
+  transform: ${({ isOpen }) =>
+    isOpen ? 'translateX(0)' : 'translateX(-100%)'};
+  filter: ${({ isOpen }) => (isOpen ? 'opacity(50%)' : 'opacity(0%)')};
+  transition: filter 150ms ease-out,
+    ${({ isOpen }) => (isOpen ? 'transform 0ms' : 'transform 0ms ease 150ms')};
+  background-color: #000;
+  z-index: 150;
+  will-change: transform;
 `;
 
-const InnerContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
+const Container = styled.div`
+  position: fixed;
   transform: ${({ isOpen }) =>
     isOpen ? 'translateX(0%)' : 'translateX(-100%)'};
+  transition: transform 150ms ease-out;
   width: 75vw;
   height: 100%;
   background-color: ${({ theme }) => theme.colors.white};
-  transition: transform 300ms ease-out;
   z-index: 151;
+  will-change: transform;
 `;
