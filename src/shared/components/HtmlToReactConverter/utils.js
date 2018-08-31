eef6216b0b09351be2c92d1f58ece7a6c585a924
David Arenas <darensr@gmail.com>
2018-08-31 11:38:48 +0200

    refactor(h2r-utils): refactor util functions

--- src/shared/components/HtmlToReactConverter/utils.js
+++ src/shared/components/HtmlToReactConverter/utils.js
@@ -5,41 +5,54 @@ import { camelCase, capitalize } from 'lodash';
 // to the old Himalaya AST Specification v0, used by converters and processors.
 // See https://github.com/andrejewski/himalaya/blob/v1.0.1/text/ast-spec-v1.md
 // and https://github.com/andrejewski/himalaya/blob/v1.0.1/text/ast-spec-v0.md
-const adaptNode = element => {
-  const isData = /^data-(.*)/;
-  const attributes = {};
-  element.type = capitalize(element.type);
-  if (element.attributes && element.attributes.length > 0) {
-    element.attributes.forEach(({ key, value }) => {
-      const match = isData.exec(key);
-      if (key === 'class') {
-        attributes.className = value.split(' ');
-      } else if (match) {
-        if (!attributes.dataset) attributes.dataset = {};
-        attributes.dataset[camelCase(match[1])] = value;
-      } else attributes[key] = value;
-    });
-    element.attributes = attributes;
-  } else {
-    element.attributes = {};
-  }
-  return element;
-};
-
 export const adaptNodes = nodes =>
-  nodes.map(n => {
-    if (n.children) n.children = adaptNodes(n.children);
-    return adaptNode(n);
+  nodes.map(node => {
+    // First adapt children nodes
+    if (node.children) node.children = adaptNodes(node.children);
+
+    // Then adapt the current node
+    const isData = /^data-(.*)/;
+    const attributes = {};
+    node.type = capitalize(node.type);
+
+    if (node.attributes && node.attributes.length > 0) {
+      node.attributes.forEach(({ key, value }) => {
+        const match = isData.exec(key);
+        if (key === 'class') {
+          attributes.className = value.split(' ');
+        } else if (match) {
+          if (!attributes.dataset) attributes.dataset = {};
+          attributes.dataset[camelCase(match[1])] = value;
+        } else attributes[key] = value;
+      });
+      node.attributes = attributes;
+    } else {
+      node.attributes = {};
+    }
+    return node;
   });
 
-export const removeEmptyNodes = nodes =>
+export const cleanNodes = nodes =>
   nodes.filter(node => {
     if (node.type === 'element') {
-      node.children = removeEmptyNodes(node.children);
+      node.children = cleanNodes(node.children);
       return true;
     }
-    return node.content.trim().length;
+    return !(
+      node.type === 'comment' ||
+      (node.type === 'text' && !node.content.trim().length) ||
+      (node.type === 'element' && node.tagName.toLowerCase() === '!doctype') ||
+      (node.type === 'element' && node.tagName.toLowerCase() === 'head')
+    );
   });
+
+export const extractFromBody = nodes =>
+  nodes.reduce((result, element) => {
+    if (['html', 'body'].includes(element.tagName)) {
+      result.splice(result.length, 0, element.children);
+    } else result.push(element);
+    return result;
+  }, []);
 
 export const isValidReact = element =>
   React.isValidElement(element) ||
