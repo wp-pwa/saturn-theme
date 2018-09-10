2d8de939b8fa1cd51fb2ab767fff94a292eadc58
David Arenas <darensr@gmail.com>
2018-09-10 12:11:26 +0200

    refactor(stores): make processors volatile

--- src/shared/stores/h2r.js
+++ src/shared/stores/h2r.js
@@ -8,22 +8,19 @@ const priorityValues = {
 
 export default types
   .model('h2r')
-  .props({
-    processors: types.optional(types.array(types.frozen), []),
-  })
+  .volatile(() => ({
+    processors: [],
+  }))
   .views(self => ({
     get processorsByPriority() {
       return self.processors.sort((p1, p2) => p1.priority - p2.priority);
     },
   }))
   .actions(self => ({
-    addProcessor(proc, priority) {
-      Object.defineProperty(proc, 'priority', {
-        writable: true,
-        value:
-          typeof priority === 'string' ? priorityValues[priority] : priority,
-      });
+    addProcessor(processor, priority) {
+      const priorityValue =
+        typeof priority === 'string' ? priorityValues[priority] : priority;
 
-      self.processors.push(proc);
+      self.processors.push({ priority: priorityValue, ...processor });
     },
   }));
