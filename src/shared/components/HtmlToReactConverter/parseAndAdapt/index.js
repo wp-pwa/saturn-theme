c636d9f1d76d75e76b4ce59c8e4e1b8f64cda2f9
David Arenas <darensr@gmail.com>
2018-09-17 11:30:50 +0200

    fix(parse-and-adapt): fix htmlTree adapt algorithm

--- src/shared/components/HtmlToReactConverter/parseAndAdapt/index.js
+++ src/shared/components/HtmlToReactConverter/parseAndAdapt/index.js
@@ -2,18 +2,25 @@ import { parse } from 'himalaya';
 import filterAttributes from './filterAttributes';
 
 // Adapts the new Himalaya AST Specification v1
-// to the old Himalaya AST Specification v0, used by converters and processors.
-// See https://github.com/andrejewski/himalaya/blob/v1.0.1/text/ast-spec-v1.md
-// and https://github.com/andrejewski/himalaya/blob/v1.0.1/text/ast-spec-v0.md
+// (see https://github.com/andrejewski/himalaya/blob/v1.0.1/text/ast-spec-v1.md)
+// to our format, with the following structure:
+// {
+//   type: 'element',
+//   component: 'p',
+//   props: {},
+//   children: [],
+//   parent: Element,
+// }
+
 const adaptNode = (element, parent) => {
-  const { type, tagName, attributes, children } = element;
+  const { type, tagName, attributes, children = [] } = element;
 
   // do not transform texts or comments
   if (type !== 'element') {
     return { ...element, parent };
   }
 
-  return {
+  const adapted = {
     type,
     component: tagName,
     props: filterAttributes(
@@ -24,15 +31,12 @@ const adaptNode = (element, parent) => {
         return attrs;
       }, {}),
     ),
-    children,
     parent,
   };
+
+  adapted.children = children.map(el => adaptNode(el, adapted));
+
+  return adapted;
 };
 
-const adaptNodes = (nodes, parent = null) =>
-  nodes.map(n => {
-    if (n.children) n.children = adaptNodes(n.children, n);
-    return adaptNode(n, parent);
-  });
-
-export default html => adaptNodes(parse(html));
+export default html => parse(html).map(el => adaptNode(el));
