d90f41afa30baec16e0babdb9bae9f5d72cff131
David Arenas <darensr@gmail.com>
2018-09-25 10:46:07 +0200

    refactor(helpers): remove unused code

--- src/shared/helpers/index.js
+++ src/shared/helpers/index.js
@@ -96,52 +96,6 @@ export const getThemeProps = color => ({
   logoFontSize: '1.3rem',
 });
 
-// This function iterates the element object recursively until it finds an 'element'
-// with component 'a' and its 'href' attribute matches a RegExp that captures a tweet ID.
-export const getTweetId = children => {
-  if (!children) return '';
-
-  const results = [];
-
-  for (let i = 0; i < children.length; i += 1) {
-    const child = children[i];
-
-    if (child.type === 'element' && child.component === 'a') {
-      const match = child.props.href.match(/\/status\/(\d+)/);
-
-      if (match) return match[1];
-    }
-
-    if (child.children) results.push(getTweetId(child.children));
-  }
-
-  return results.reduce((result, current) => current || result, '');
-};
-
-// This function iterates the element object recursively until it finds an 'element'
-// with component 'a' and its 'href' attribute matches a RegExp that captures an instagram ID.
-export const getInstagramId = children => {
-  if (!children) return '';
-
-  const results = [];
-
-  for (let i = 0; i < children.length; i += 1) {
-    const child = children[i];
-
-    if (child.type === 'element' && child.component === 'a') {
-      const match = child.props.href.match(
-        /https:\/\/www\.instagram\.com\/p\/([\w\d]+)/,
-      );
-
-      if (match) return match[1];
-    }
-
-    if (child.children) results.push(getInstagramId(child.children));
-  }
-
-  return results.reduce((result, current) => current || result, '');
-};
-
 // Request for JSONP endpoint.
 export const getContent = endpoint =>
   new Promise((resolve, reject) => {
