81a7d85ba0dcbf5aaefb50ed5e9896b5073fe22e
David Arenas <darensr@gmail.com>
2018-09-26 17:44:18 +0200

    fix(processors): fix some errors in image processor

--- src/shared/processors/low/image.js
+++ src/shared/processors/low/image.js
@@ -1,26 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
 import Link from '../../../pwa/components/Link';
 import Image from '../../components/Image';
 import { media } from '../../contexts';
 
 export default {
-  test: element => {
-    const { component, ignore } = element;
-    // Returns false if it's already a lazy component.
-    if (ignore) return false;
-
-    // Returns true if element is an <img>.
-    if (component === 'img') return true;
-
-    // Filters comments out of children.
-    return false;
-  },
+  test: ({ component, ignore }) => !ignore && component === 'img',
   process: (element, { stores: { settings }, item }) => {
     const { props } = element;
     const { 'data-attachment-id': attachmentId } = props;
-
-    let ImageComponent;
 
     // Return an Image component with id if image has attachedId.
     if (attachmentId) {
@@ -32,88 +19,97 @@ export default {
           return final;
         }, []);
 
-      ImageComponent = () => (
-        <Link
-          type="media"
-          id={id}
-          context={media(contentContext || [])}
-          eventCategory="Post"
-          eventAction="open content media"
-        >
-          <a>
-            <Image
-              isContent
-              key={attachmentId}
-              id={id}
-              lazyloadContainerSelector=".content"
-            />
-          </a>
-        </Link>
-      );
+      return {
+        type: 'element',
+        component: Link,
+        props: {
+          id,
+          type: 'media',
+          context: media(contentContext || []),
+          eventCategory: 'Post',
+          eventAction: 'open content media',
+        },
+        children: [
+          {
+            type: 'element',
+            component: 'a',
+            props: {},
+            children: [
+              {
+                type: 'element',
+                component: Image,
+                props: {
+                  id,
+                  key: attachmentId,
+                  isContent: true,
+                  lazyloadContainerSelector: '.content',
+                },
+                children: null,
+              },
+            ],
+          },
+        ],
+      };
+    }
+
+    const alt = props.alt || null;
+
+    let src = null;
+
+    // Get src attribute from different cases.
+    if (props.src && typeof props.src === 'string') {
+      if (props.src.startsWith('/'))
+        src = `${settings.generalSite.url}${props.src}`;
+      else ({ src } = props);
+    } else if (
+      props['data-original'] &&
+      typeof props['data-original'] === 'string'
+    ) {
+      if (props.src.startsWith('/'))
+        src = `${settings.generalSite.url}${props['data-original']}`;
+      else src = props['data-original'];
+    }
+
+    let srcSet = null;
+
+    // Get srcset attribute from different cases.
+    if (props.srcSet && typeof props.srcSet === 'string') {
+      srcSet = props.srcSet
+        .split(',')
+        .map(s => {
+          const trimmed = s.trim();
+
+          if (trimmed.startsWith('/'))
+            return `${settings.generalSite.url}${trimmed}`;
+
+          return trimmed;
+        })
+        .join(', ');
+    }
+
+    let height;
+
+    // Calculate width and height.
+    if (props.height && props.width) {
+      height = `${100 * (props.height / props.width)}vw`;
     } else {
-      const alt = props.alt || null;
-
-      let src = null;
-
-      // Get src attribute from different cases.
-      if (props.src && typeof props.src === 'string') {
-        if (props.src.startsWith('/'))
-          src = `${settings.generalSite.url}${props.src}`;
-        else ({ src } = props);
-      } else if (
-        props['data-original'] &&
-        typeof props['data-original'] === 'string'
-      ) {
-        if (props.src.startsWith('/'))
-          src = `${settings.generalSite.url}${props['data-original']}`;
-        else src = props['data-original'];
-      }
-
-      let srcSet = null;
-
-      // Get srcset attribute from different cases.
-      if (props.srcset && typeof props.srcset === 'string') {
-        srcSet = props.srcset
-          .split(',')
-          .map(s => {
-            const trimmed = s.trim();
-
-            if (trimmed.startsWith('/'))
-              return `${settings.generalSite.url}${trimmed}`;
-
-            return trimmed;
-          })
-          .join(', ');
-      }
-
-      let height;
-
-      // Calculate width and height.
-      if (props.height && props.width) {
-        height = `${100 * (props.height / props.width)}vw`;
-      } else {
-        height = 'auto';
-      }
-
-      ImageComponent = () => (
-        <Image
-          key={src}
-          isContent
-          width="100vw"
-          height={height}
-          alt={alt}
-          src={src}
-          srcSet={srcSet}
-          lazyloadContainerSelector=".content"
-        />
-      );
+      height = 'auto';
     }
 
     return {
-      type: 'Element',
-      component: ImageComponent,
-      props: {},
-      children: [],
+      type: 'element',
+      component: Image,
+      props: {
+        isContent: true,
+        key: src,
+        width: '100vw',
+        height,
+        alt,
+        src,
+        srcSet,
+        lazyloadContainerSelector: '.content',
+      },
+      children: null,
     };
   },
 };
