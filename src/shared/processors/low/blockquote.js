02a124f90abdb864375787d06728804fff771a6c
David Arenas <darensr@gmail.com>
2018-09-27 11:27:34 +0200

    refactor(processors): take Blockquote component out of its processor

--- src/shared/processors/low/blockquote.js
+++ src/shared/processors/low/blockquote.js
@@ -1,21 +1,6 @@
-import Color from 'color-js';
-import styled from 'styled-components';
-import { inject } from 'mobx-react';
-
-const Blockquote = styled.blockquote`
-  background: ${({ color, theme }) =>
-    Color(color || theme.colors.evilGrey)
-      .setAlpha(0.2)
-      .toString()};
-  border-left: 0.25rem solid
-    ${({ color, theme }) => color || theme.colors.evilGrey};
-`;
-
-const InjectedBlockquote = inject(({ stores: { settings: { theme } } }) => ({
-  color: theme.blockquoteStyles && theme.blockquoteStyles.color,
-}))(Blockquote);
+import Blockquote from '../../styled/Blockquote';
 
 export default {
   test: ({ component }) => component === 'blockquote',
-  process: () => ({ component: InjectedBlockquote }),
+  process: () => ({ component: Blockquote }),
 };
