79e18be72bf9a11b67a8ac2767bf837d4b524612
David Arenas <darensr@gmail.com>
2018-11-14 16:24:21 +0100

    fix(wp-embeds): delete 'removeWpEmbeddedContent' processor

--- src/shared/processors/low/index.js
+++ src/shared/processors/low/index.js
@@ -6,12 +6,10 @@ import gallery from './gallery';
 import wpappbox from './wpappbox';
 import anchor from './anchor';
 import table from './table';
-import removeWpEmbeddedContent from './removeWpEmbeddedContent';
 
 export default [
   removeContentAds,
   removeArticles,
-  removeWpEmbeddedContent,
   anchor,
   image,
   blockquote,
